b2538958b1ca5553f08473c5cc772c7f864c3215
Michael Bullington <mikebullingtn@gmail.com>
2015-06-01 21:41:41 -0400

    Fix bugs.

--- lib/src/analysis/visitor.dart
+++ lib/src/analysis/visitor.dart
@@ -23,7 +23,7 @@ class Visitor extends GeneralizingAstVisitor<dynamic> {
 }
 
 class VisitorBuilder {
-  final Map<Type, List<VisitorFunction>> _visitors = [];
+  final Map<Type, List<VisitorFunction>> _visitors = {};
 
   void where(types, VisitorFunction visitor) {
     if(types is Iterable<Type>) {
@@ -44,41 +44,5 @@ class VisitorBuilder {
 
   Visitor build(data) {
     return new Visitor(data, _visitors);
-  }
-}
-
-/*
-class _ParamAstVisitor extends GeneralizingAstVisitor<dynamic> {
-  final List output;
-
-  _ParamAstVisitor(this.output);
-
-  @override
-  visitNode(AstNode node) {
-    if(node is DefaultFormalParameter || node is NormalFormalParameter) {
-      var norm = node;
-      if(node is DefaultFormalParameter)
-        norm = norm.parameter;
-
-      if(norm is FunctionTypedFormalParameter) {
-        var types = [];
-        var params = [];
-        norm.parameters.visitChildren(new _ParamAstVisitor(params));
-        for(var param in params) {
-          types.add(param.type != null ? param.type : "dynamic");
-        }
-
-        types.add(norm.returnType != null ? norm.returnType.toString() : "void");
-
-        output.add(new Parameter(norm.kind, "Function<${types.join(",")}>", norm.identifier.toString()));
-        return;
-      }
-
-      output.add(new Parameter(norm.kind,
-          norm.type.toString(),
-          norm.identifier.toString(),
-          node is DefaultFormalParameter && node.defaultValue is Literal ? node.defaultValue.toString() : "null"));
-      return;
-    }
   }
 }
