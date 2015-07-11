3b6557873d2fcb75d9c01ab498f9332a07d9a918
Michael Bullington <mikebullingtn@gmail.com>
2015-07-10 20:17:24 -0400

    Make inheriting methods compatible with ES6 super.

--- test/output_test.js
+++ test/output_test.js
@@ -129,6 +129,7 @@ describe('inheritance', function() {
 
   assert(test.invoke() === 'Salutations, human.');
   assert(test.c.invoke() === 'Salutations, human.');
+  assert(_super.prototype.invoke.call(test.c) === 'Hello World!');
 
   InheritanceTest.prototype.invoke = function() {
     return this.str;
@@ -152,4 +153,5 @@ describe('retain_wrapper_instance', function() {
 
   assert(test.c.abc === 'xyz');
 });
+
 console.log(JSON.stringify(json));
